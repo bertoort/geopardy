9954fc6a0b662f44e8d019eb99aa02bf83213231
Roberto Ortega <berto.ort@gmail.com>
2015-08-28 15:41:11 -0600

    team submission into firebase added

--- client/assets/js/app.js
+++ client/assets/js/app.js
@@ -5,6 +5,7 @@
   var app = angular.module('application', [
     'ui.router',
     'ngAnimate',
+    'firebase',
 
     //foundation
     'foundation',
@@ -14,10 +15,29 @@
     app.config(config)
     app.run(run)
     app.controller('login', login)
+    app.controller('header', header)
+    app.controller('trivia', trivia)
   ;
 
+  app.factory('cookie', function () {
+    var cookie;
+
+    var obj = {
+      cookie: function () {
+        return cookie
+      },
+      addCookie: function (ck) {
+        cookie = ck;
+      }
+    }
+
+    return obj
+  })
+
   config.$inject = ['$urlRouterProvider', '$locationProvider'];
-  login.$inject = ['$scope'];
+  login.$inject = ['$scope', 'cookie', '$firebaseArray'];
+  header.$inject = ['$scope', 'cookie'];
+  trivia.$inject = ['$scope', 'cookie', '$firebaseArray'];
 
   function config($urlProvider, $locationProvider) {
     $urlProvider.otherwise('/');
@@ -34,21 +54,58 @@
     FastClick.attach(document.body);
   }
 
-  function login($scope) {
+  function login($scope, Cookie, $firebaseArray) {
+    var geopardyRef = new Firebase("https://geopardy.firebaseio.com/");
+    $scope.geopardy = $firebaseArray(geopardyRef);
     $scope.message = "hello";
-    $scope.cookie = $.cookie().team;
+    Cookie.addCookie($.cookie().team);
+    $scope.cookie = Cookie.cookie();
     $scope.login = function () {
       $.cookie('team', $scope.team);
-      console.log($scope.cookie);
+      $scope.cookie = Cookie.cookie();
+      $scope.geopardy.$add({name: $scope.team, score: 0, answered: false, explained: false, currentAnswer: '', currentExplanation: '', editExplanation: true, editAnswer: true, answerSubmissionTime: ''}).then(function () {
+        window.location.href = '/';
+      })
     }
   }
 
+  function header($scope, Cookie) {
+    Cookie.addCookie($.cookie().team);
+    $scope.cookie = Cookie.cookie();
+    $scope.logout = function () {
+      $scope.cookie = null;
+      $.removeCookie('team');
+    }
+  }
 
-
-  //
-  // app.controller('login', [ '$scope', '$cookies', function ($scope, $cookies) {
-  //   $scope.message = "hello";
-    // $cookies.put('myFavorite', 'oatmeal');
-  // }])
+  function trivia($scope, Cookie, $firebaseArray) {
+    $scope.cookie = Cookie.cookie();
+    var geopardyRef = new Firebase("https://geopardy.firebaseio.com/");
+    $scope.geopardy = $firebaseArray(geopardyRef);
+    $scope.submitAnswer = function (answer, team) {
+      var time = new Date();
+      team.currentAnswer = '';
+      team.currentAnswer = answer;
+      team.answered = true;
+      team.editAnswer = false;
+      team.answerSubmissionTime = String(time);
+      $scope.geopardy.$save(team);
+    }
+    $scope.submitExplanation = function (explanation, team) {
+      team.currentExplanation = '';
+      team.currentExplanation = explanation;
+      team.editExplanation = false;
+      team.explaned = true;
+      $scope.geopardy.$save(team);
+    }
+    $scope.editExplanation = function (team) {
+      team.editExplanation = true;
+      $scope.geopardy.$save(team);
+    }
+    $scope.editAnswer = function (team) {
+      team.editAnswer = true;
+      $scope.geopardy.$save(team);
+    }
+  }
 
 })();
